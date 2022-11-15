f2129579ef2617b2a43512eb3522c3158a3d044f
EmannuelMartinez <martinez.emannuel@gmail.com>
2022-11-14 19:46:04 -0800

    Add a file I can use to write random JS

---
+++ getting-started/script.js
@@ -0,0 +1,32 @@
+/**
+ * The code below tells the browser to ask you for a number
+ * then if that number is `6`, it returns `true` otherwise it returns `false`
+ *
+ * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
+ */
+
+number = Number(prompt("enter a number"));
+
+function numberChecker() {
+  return number >= 10;
+}
+
+module.exports = {
+  testGroup: "a",
+  greeting,
+  birthYear,
+  thisYear,
+  firstName,
+  lastName,
+};
+
+module.exports = {
+  testGroup: "b",
+  greeting,
+  birthYear,
+  thisYear,
+  firstName,
+  lastName,
+  fullName,
+  age,
+};
